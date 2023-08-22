910261ce9509b04de0dee7b59d2f64b0848dbbe6
ZhaoHc <zhaohc@eastsoft.com.cn>
2023-08-22 17:43:07 +0800

    edit

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -2,11 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class);
+        ConfigurableApplicationContext context= SpringApplication.run(GatewayApplication.class);
+
+//        System.out.println(context.getEnvironment().getPropertSources());
+//        System.out.println(context.getEnvironment().getPr);
     }
 }
--- gateway/src/test/java/Test.java
+++ gateway/src/test/java/Test.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.concurrent.locks.LockSupport;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -61,6 +63,9 @@ public class Test {
                 (array, index) -> array[index]++,
                 array-> System.out.println(Arrays.toString(array))
         );
+
+
+        ForkJoinPool forkJoinPool=new ForkJoinPool();
     }
 
 
@@ -100,4 +105,42 @@ public class Test {
         printConsumer.accept(array);
     }
 
+}
+
+
+class AddTask3 extends RecursiveTask<Integer> {
+    int begin;
+    int end;
+    public AddTask3(int begin, int end) {
+        this.begin = begin;
+        this.end = end;
+    }
+    @Override
+    public String toString() {
+        return "{" + begin + "," + end + '}';
+    }
+    @Override
+    protected Integer compute() {
+// 5, 5
+        if (begin == end) {
+            log.debug("join() {}", begin);
+            return begin;
+        }
+// 4, 5
+
+        if (end - begin == 1) {
+            log.debug("join() {} + {} = {}", begin, end, end + begin);
+            return end + begin;
+        }
+// 1 5
+        int mid = (end + begin) / 2; // 3
+        AddTask3 t1 = new AddTask3(begin, mid); // 1,3
+        t1.fork();
+        AddTask3 t2 = new AddTask3(mid + 1, end); // 4,5
+        t2.fork();
+        log.debug("fork() {} + {} = ?", t1, t2);
+        int result = t1.join() + t2.join();
+        log.debug("join() {} + {} = {}", t1, t2, result);
+        return result;
+    }
 }
