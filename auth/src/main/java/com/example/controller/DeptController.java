0b895b3c3e1c9fb68cb15ab398528927dce63789
lucius <luciusZhao.cs@gmail.com>
2023-12-10 10:57:58 +0800

    将auth下的application.yml配置信息全部放入到nacos 的auth.yaml中

--- auth/src/main/java/com/example/controller/DeptController.java
+++ auth/src/main/java/com/example/controller/DeptController.java
@@ -5,6 +5,7 @@ import com.example.pojo.Dept;
 import com.example.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 ///提供restful服务
 
 @RestController
+@RefreshScope    //用于从nacos中获取的配置信息动态刷新，要不然nacos配置信息publish后程序不刷新
 public class DeptController {
 
     @Autowired
     private DeptService deptService;
 
 //
-    @Value("${pattern.dateformat}")   //这里获取的是nacos的配置信息
+    @Value("${pattern.dateformat}")   //这里获取的是nacos的配置信息,使用的是auth-dev.yaml
     private String dateFormat;
-    @Value("${pattern.test}")   //这里获取的是nacos的配置信息
+    @Value("${pattern.test}")   //这里获取的是nacos的配置信息,使用的是auth-dev.yaml
     private String test;
+    //
+    @Value("${pattern1.dateformat1}")   //这里获取的是nacos，使用的是  auth.yaml
+    private String dateFormat1;
+    @Value("${pattern1.test1}")   //这里获取的是nacos的配置信息,使用的是auth.yaml
+    private String test1;
+
+    @Value("${allservice.config_string}")   //这里获取的是nacos，使用的是  auth.yaml
+    private String sharedInfo1;
+
 
 
     @PostMapping("dept/add")
@@ -30,7 +41,10 @@ public class DeptController {
     {
         //
         System.out.println(dateFormat);
+        System.out.println(dateFormat1);
         System.out.println(test);
+        System.out.println(test1);
+        System.out.println(sharedInfo1);
          return  deptService.addDept(new Dept(100,"123"));
 
 
---
+++ 配置README.md
@@ -0,0 +1,12 @@
+
+
+
+
+## 配置文件优先级
+
+优先级由高到低： bootstrap.properties >  bootstrap.yml > application.properties > application.yml
+
+
+## 配置共享
+1、同一个微服务不同环境间共享配置
+2、不同微服务之间共享配置
