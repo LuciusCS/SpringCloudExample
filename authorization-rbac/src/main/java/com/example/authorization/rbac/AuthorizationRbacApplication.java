5dd5f980273e902f904e3d514adad1a6b88b21b7
lucius <luciusZhao.cs@gmail.com>
2024-06-03 19:42:19 +0800

    edit

---
+++ authorization-rbac/src/main/java/com/example/authorization/rbac/AuthorizationRbacApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorization.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationRbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationRbacApplication.class, args);
+    }
+
+}
---
+++ authorization-rbac/src/test/java/com/example/authorization/rbac/AuthorizationRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorization.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationRbacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
